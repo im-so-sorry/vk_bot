8bfbdfab80bf30ac7647c975e02f22acd53f9157
Santa <223860@niuitmo.ru>
2020-04-22 04:32:25 +0300

    fix some bag

--- vkbot.py
+++ vkbot.py
@@ -21,18 +21,28 @@ def main():
             if event.text == "/start":
                 message = "Hello I can help to search mem in vk"
                 vk.messages.send(user_id=event.user_id, random_id=get_random_id(), message=message)
+                continue
             if event.text == "/help":
                 message = "/reg - регистрация сервиса\n/add_rule <tag> <value> - добавление " \
                           "правила\n/remove_rule <tag> - удаление правила\n/rules - получение списка правил"
                 vk.messages.send(user_id=event.user_id, random_id=get_random_id(), message=message)
+                continue
             if event.text == "/reg":
                 vk.messages.send(user_id=event.user_id, random_id=get_random_id(), message=message)
+                continue
             if event.text == "/add_rule":
                 vk.messages.send(user_id=event.user_id, random_id=get_random_id(), message=message)
+                continue
             if event.text == "/remove_rule":
                 vk.messages.send(user_id=event.user_id, random_id=get_random_id(), message=message)
+                continue
             if event.text == "/rule":
                 vk.messages.send(user_id=event.user_id, random_id=get_random_id(), message=message)
+                continue
+            else:
+                message = "Хм что то странное, я тебя не понимаю, попробуй команду /help"
+                vk.messages.send(user_id=event.user_id, random_id=get_random_id(), message=message)
+
 
 
 if __name__ == '__main__':
