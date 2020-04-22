06bf423500a3140c4bc853d40ef49c2a9d8ff9cf
Santa <223860@niuitmo.ru>
2020-04-22 19:02:39 +0300

    add database

--- vkbot.py
+++ vkbot.py
@@ -1,12 +1,25 @@
+from random import random
+
 import requests
-from config import token
+from config import *
 import vk_api
+import psycopg2
 from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api.utils import get_random_id
+from psycopg2.extras import DictCursor
+from psycopg2 import sql
 
 
 def main():
     session = requests.Session()
+    db = psycopg2.connect(
+        database=database,
+        user=user,
+        password=password,
+        host=host,
+        port=port
+    )
+    cursor = db.cursor(cursor_factory=DictCursor)
 
     vk_session = vk_api.VkApi(
         token=token)
@@ -17,32 +30,85 @@ def main():
     message = ""
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
+            text = event.text.split(' ')
+            if text[0] == "/start":
+                cursor.execute("SELECT id from users where vk_id = {}".format(event.user_id))
+                row = cursor.fetchall()
+                if len(row) > 0:
+                    message = "Мы с тобой уже знакомы, но еще раз здравствуй!"
+                else:
+                    cursor.execute("INSERT INTO users (vk_id) VALUES ({})".format(event.user_id))
+                    db.commit()
+                    message = "Привет я помогу тебе с поиском мемов в вк по тегам"
 
-            if event.text == "/start":
-                message = "Hello I can help to search mem in vk"
                 vk.messages.send(user_id=event.user_id, random_id=get_random_id(), message=message)
                 continue
-            if event.text == "/help":
+
+            if text[0] == "/help":
                 message = "/reg - регистрация сервиса\n/add_rule <tag> <value> - добавление " \
                           "правила\n/remove_rule <tag> - удаление правила\n/rules - получение списка правил"
                 vk.messages.send(user_id=event.user_id, random_id=get_random_id(), message=message)
                 continue
-            if event.text == "/reg":
+
+            if text[0] == "/reg":
+                if len(text) > 2:
+                    message = "Одновременно можно активировать только один аккаунт."
+                    vk.messages.send(user_id=event.user_id, random_id=get_random_id(), message=message)
+                    continue
+
+                cursor.execute("SELECT * FROM users WHERE telegram_id = {}".format(text[1]))
+                if len(cursor.fetchall()) > 0:
+                    message = "Данный аккаунт уже зарегистрирован"
+                else:
+                    cursor.execute("SELECT id from registry where reg_id = {}".format(text[1]))
+                    row = cursor.fetchall()
+                    ran_gen = random.randint(1000, 9999)
+                    if len(row) > 0:
+                        cursor.execute("UPDATE registry SET access_code = {} WHERE reg_id = {}".format(ran_gen, text[1]))
+                        message = "Ты уже регистрировался, но еще не активировал свой аккаунт. Перейди по ссылке t.me " \
+                                  "и введи новый код /activate {}".format(ran_gen)
+                    else:
+                        cursor.execute(
+                            "INSERT INTO registry (id, access_token, reg_id) VALUES ({}, {}, {}})".format(event.user_id, ran_gen, text[1]))
+                        message = "Хорошо, теперь тебе надо активировать этот аккаунт перейди по сыылке и введи: " \
+                                  "/activate {}".format(ran_gen)
+
                 vk.messages.send(user_id=event.user_id, random_id=get_random_id(), message=message)
                 continue
-            if event.text == "/add_rule":
+
+            if text[0] == "/add_rule":
+                # text.remove("/add_rule")
+                for i in range(1, len(text), 2):
+                    a = 0
                 vk.messages.send(user_id=event.user_id, random_id=get_random_id(), message=message)
                 continue
-            if event.text == "/remove_rule":
+
+            if text[0] == "/remove_rule":
                 vk.messages.send(user_id=event.user_id, random_id=get_random_id(), message=message)
                 continue
-            if event.text == "/rule":
+
+            if text[0] == "/rule":
                 vk.messages.send(user_id=event.user_id, random_id=get_random_id(), message=message)
                 continue
-            else:
-                message = "Хм что то странное, я тебя не понимаю, попробуй команду /help"
+
+            if text[0] == "/activate":
+                if len(text) > 2:
+                    message = "Неверный код активации"
+                else:
+                    cursor.execute("SELECT * from registry where reg_id = {}".format(event.user_id))
+                    row = cursor.fetchone()
+                    if row['access_code'] == text[1]:
+                        message = "Успешно зарегистрирован"
+                        cursor.execute("UPDATE users SET vk_id = {} WHERE telegram_id = {}".format(event.user_id, row['user_id']))
+                    else:
+                        message = "Неверный код"
                 vk.messages.send(user_id=event.user_id, random_id=get_random_id(), message=message)
 
+            if (text[0] == "./stop") & (text[1] == stopCode):
+                return
+
+            message = "Хм что то странное, я тебя не понимаю, попробуй команду /help"
+            vk.messages.send(user_id=event.user_id, random_id=get_random_id(), message=message)
 
 
 if __name__ == '__main__':
